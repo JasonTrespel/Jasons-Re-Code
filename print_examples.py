a3d9ae64e7d7a9db23443b0c2bc94dc3523de2a6
JasonTrespel <jason.trespel11@gmail.com>
2021-05-26 03:33:12 +0000

    added print examples

---
+++ print_examples.py
@@ -0,0 +1,24 @@
+# print an integer
+print(987)
+
+# print a string
+print("Hellooo!")
+# We know it's a string because it has quotes around it.
+
+# print 2 strings 
+print("Help", "me think!")
+
+#print int and str
+print(759, "This is my IQ.")
+
+# cd = change directory
+
+# set variable and print
+tree_height = 82
+print(tree_height)
+
+print("A tree is", tree_height)
+print("A tree is", tree_height, "feet high.")
+tree_height = 178
+
+print("A tree is", tree_height, "feet high.")
